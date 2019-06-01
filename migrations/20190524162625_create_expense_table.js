cf4114e377805de0cab6333aaae133fd3222861e
Abhansh <abhanshgiri21@gmail.com>
2019-06-01 14:41:32 +0530

    Minor

--- migrations/20190524162625_create_expense_table.js
+++ migrations/20190524162625_create_expense_table.js
@@ -1,8 +1,29 @@
-
-exports.up = function(knex, Promise) {
-  
+exports.up = knex => {
+    return knex.schema.createTable('category', table => {
+        table.increments('id').primary();
+        table
+            .integer('category_id')
+            .unsigned()
+            .nullable()
+            .references('id')
+            .inTable('category')
+            .onDelete('CASCADE');
+        table
+            .string('name')
+            .notNullable();
+        table
+            .string('desc')
+            .notNullable();
+        table
+            .integer('amount')
+            .unsigned();
+        table
+            .integer('')
+            .unsigned();
+        table.timestamps(false, true);
+    })
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = knex => {
+
 };
