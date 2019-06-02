6b4df3d2ad1c7580e0f4d6d21fada2440aa07b48
Mayank Dubey <mayankdubey291@gmail.com>
2019-06-02 23:14:30 +0530

    Remove schema name variable

--- migrations/20190524113907_create_category_table.js
+++ migrations/20190524113907_create_category_table.js
@@ -1,27 +1,27 @@
 exports.up = knex => {
-    return knex.schema.createTable('category', table => {
-        table.increments('id').primary();
-        table
-            .integer('parentId')
-            .unsigned()
-            .nullable()
-            .references('id')
-            .inTable(schemaName + '.category')
-            .onDelete('CASCADE');
-        table
-            .string('name')
-            .notNullable();
-        table
-            .string('slug')
-            .notNullable();
-        table
-            .integer('treeId')
-            .unsigned();
-        table
-            .integer('level')
-            .unsigned();
-        table.timestamps(false, true);
-    })
+  return knex.schema.createTable('category', table => {
+    table.increments('id').primary();
+    table
+      .integer('parentId')
+      .unsigned()
+      .nullable()
+      .references('id')
+      .inTable('category')
+      .onDelete('CASCADE');
+    table
+      .string('name')
+      .notNullable();
+    table
+      .string('slug')
+      .notNullable();
+    table
+      .integer('treeId')
+      .unsigned();
+    table
+      .integer('level')
+      .unsigned();
+    table.timestamps(false, true);
+  })
 };
 
 exports.down = knex => {
