10009910ac3bead49378f763c77835dab48c56d0
Mayank Dubey <mayankdubey291@gmail.com>
2019-06-02 23:14:49 +0530

    Change table name from expense to category

--- migrations/20190524162625_create_expense_table.js
+++ migrations/20190524162625_create_expense_table.js
@@ -1,27 +1,24 @@
 exports.up = knex => {
-    return knex.schema.createTable('category', table => {
-        table.increments('id').primary();
-        table
-            .integer('category_id')
-            .unsigned()
-            .nullable()
-            .references('id')
-            .inTable('category')
-            .onDelete('CASCADE');
-        table
-            .string('name')
-            .notNullable();
-        table
-            .string('desc')
-            .notNullable();
-        table
-            .integer('amount')
-            .unsigned();
-        table
-            .integer('')
-            .unsigned();
-        table.timestamps(false, true);
-    })
+  return knex.schema.createTable('expense', table => {
+    table.increments('id').primary();
+    table
+      .integer('category_id')
+      .unsigned()
+      .nullable()
+      .references('id')
+      .inTable('category')
+      .onDelete('CASCADE');
+    table
+      .string('name')
+      .notNullable();
+    table
+      .string('desc')
+      .notNullable();
+    table
+      .integer('amount')
+      .unsigned();
+    table.timestamps(false, true);
+  })
 };
 
 exports.down = knex => {
