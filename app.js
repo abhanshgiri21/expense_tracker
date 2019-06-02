97895ece2139ba5f4923859181766a7945c1822d
Mayank Dubey <mayankdubey291@gmail.com>
2019-06-02 13:32:41 +0530

    Refactor code in entry file

--- app.js
+++ app.js
@@ -1,29 +1,20 @@
 require('dotenv').config();
-const createError = require('http-errors');
+require('./global_functions');
+
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const Knex = require('knex');
 const Model = require('objection').Model;
-const passport = require('passport');
-const fs = require('fs');
 const KnexConfig = require('./knexfile');
 const api = require('./api');
-let router = require('express-promise-router')();
+const router = require('express-promise-router')();
 
 // Global Functions accessible all across the app
-require('./global_functions');
 
-const knex = Knex(KnexConfig[process.env.NODE_ENV || 'production']);
+const knex = Knex(KnexConfig[process.env.NODE_ENV || 'databaseConfig']);
 Model.knex(knex);
-knex.migrate.latest().then((res) => {
-  console.log('migrations done : ', res);
-  // knex.seed.run();
-}).catch(err => {
-  console.log('error running migrations : ', err);
-})
-
 
 const app = express();
 
@@ -35,22 +26,11 @@ app.use(express.urlencoded({
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(router);
+
 // register API's
 api(router);
 
-app.use((req, res, next) => {
-  // console.log('hererererererere');
-  return res.status(404).json({
-    message: 'Not Found'
-  });
-});
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
 // error handler
-
 app.use((err, req, res, next) => {
   if (err) {
     console.error(err);
@@ -59,4 +39,5 @@ app.use((err, req, res, next) => {
     next();
   }
 });
+
 module.exports = app;
